154a764cfd1b00a5f6f6d3838c60387567258003
eightprimesep22 <eightprimesep22@gmail.com>
2022-12-31 16:09:57 +0000

    Today's 31/12/2022 class update

--- src/test/java/com/studentapp/studentinfo/StudentPatchTest.java
+++ src/test/java/com/studentapp/studentinfo/StudentPatchTest.java
@@ -2,7 +2,10 @@ package com.studentapp.studentinfo;
 
 import com.studentapp.model.StudentPojo;
 import com.studentapp.testbase.TestBase;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 
 /*
  *  Created by Jay
@@ -11,8 +14,16 @@ public class StudentPatchTest extends TestBase {
 
     @Test
     public void updateStudentWithPatch(){
+        //Homework
         StudentPojo studentPojo = new StudentPojo();
         studentPojo.setEmail("prime1245@gmail.com");
-        //Homework
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .pathParam("id", 1)
+                .body(studentPojo)
+                .when()
+                .patch("/{id}");
+        response.then().statusCode(201);
+        response.prettyPrint();
     }
 }
